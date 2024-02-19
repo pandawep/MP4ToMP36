bc2da5b9fab99f83870e3f022e5989022da29196
pandawep <154017398+pandawep@users.noreply.github.com>
2024-02-19 22:16:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
+import os
+
 # Telegram API credentials
-API_ID = 'your_api_id'
-API_HASH = 'your_api_hash'
+API_ID = os.environ.get('API_ID')
+API_HASH = os.environ.get('API_HASH')
 
 # Telegram Bot token
-BOT_TOKEN = 'your_bot_token'
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
